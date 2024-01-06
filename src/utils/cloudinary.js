618d84bb9868083916e9a8b17c8faea35b302505
Mohitkmr141 <mohitcod141@gmail.com>
2024-01-06 19:32:24 +0530

    use cloudinary and multer

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,36 +1,25 @@
-import { v2 as Mohit } from "cloudinary";
+import {v2 as cloudinary} from ''
 import fs from 'fs'
 
-cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_NAME ,
-    api_key:  process.env.CLOUDINARY_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET
-  });
-
-  const uploadonCloudnary   =  async (localfilePath)=>{
-
-    try{
-            if(!localfilePath) return null ;
-
-               const response =  await cloudinary.uploader.upload(localfilePath , {
-                    resource_type:"auto"
-                })
 
           
-            console.log('file is uploaded on cloudinary' , response.url)
-            return response;
-    }   
-    catch(err){
-        fs.unlinkSync(localfilePath)
-        //remove the temporary file
-        return null
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+const uploadonClodinary = async(localFilePath)=>{
+try{
+  if(!localFilePath) return null;
+  const response = await cloudinary.uploader.upload(localFilePath , {
+    resource_type : 'auto',
+  })
+  console.log("file is uploaded on cloudinary " , response.url)
+  return response;
 
-    }
-  }
-/*
-  cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" }, 
-  function(error, result) {console.log(result); });
-  */
+}catch(err){
 
-  export {uploadonCloudnary}
+  fs.unlinkSync(localFilePath)
+}
+}
+
